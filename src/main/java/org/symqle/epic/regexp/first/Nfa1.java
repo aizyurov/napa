9acc0937f7bb091e2f6883a8b1980036f0b3d798
lvovich <lvovich@yandex-team.ru>
2017-09-29 21:17:13 +0300

    Continued

---
+++ src/main/java/org/symqle/epic/regexp/first/Nfa1.java
@@ -0,0 +1,15 @@
+package org.symqle.epic.regexp.first;
+
+/**
+ * @author lvovich
+ */
+public class Nfa1 {
+
+    private final NfaNode1 startNode;
+
+    public Nfa1(final NfaNode1 startNode) {
+        this.startNode = startNode;
+    }
+
+
+}
