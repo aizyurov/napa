d442f47008745b07c2608d3072dda83e20cb9f9e
Alexander Izyurov <aizyurov@symqle.org>
2017-10-04 22:10:01 +0300

    Add analyser

---
+++ src/main/java/org/symqle/epic/analyser/lexis/GaTokenType.java
@@ -0,0 +1,18 @@
+package org.symqle.epic.analyser.lexis;
+
+/**
+ * Created by aizyurov on 10/4/17.
+ */
+public enum GaTokenType {
+    IDENTIFIER,
+    SEMICOLON,
+    EQUALS,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    EXCLAMATION,
+    IGNORE,
+    STRING,
+    BAR
+}
---
+++ src/test/java/org/symqle/epic/grammar/GaLexerTest.java
@@ -0,0 +1,26 @@
+package org.symqle.epic.grammar;
+
+import junit.framework.TestCase;
+import org.symqle.epic.analyser.lexis.GaLexer;
+import org.symqle.epic.analyser.lexis.GaTokenType;
+import org.symqle.epic.tokenizer.PackedDfa;
+import org.symqle.epic.tokenizer.Token;
+import org.symqle.epic.tokenizer.Tokenizer;
+
+import java.io.StringReader;
+
+/**
+ * Created by aizyurov on 10/4/17.
+ */
+public class GaLexerTest extends TestCase {
+
+    public void testCompile() throws Exception {
+        final PackedDfa<GaTokenType> dfa = new GaLexer().compile();
+        String sample = "! \"[ \\n\\r]+\";\n" +
+                "unit = { class_definition | interface_definition };";
+        final Tokenizer<GaTokenType> tokenizer = new Tokenizer<GaTokenType>(dfa, new StringReader(sample));
+        for (Token<GaTokenType> token = tokenizer.nextToken(); token != null; token = tokenizer.nextToken()) {
+            System.out.println(token);
+        }
+    }
+}
