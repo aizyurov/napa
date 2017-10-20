8f4838e78a8924a9bd7c0cf506cb0fd85e20a73c
lvovich <lvovich@yandex-team.ru>
2017-10-20 15:40:32 +0300

    More tests

--- src/test/java/org/symqle/epic/grammar/javalang/LiteralTest.java
+++ src/test/java/org/symqle/epic/grammar/javalang/LiteralTest.java
@@ -16,22 +16,32 @@ import java.util.Set;
  * @author lvovich
  */
 public class LiteralTest extends TestCase {
+    
+    private final CompiledGrammar g;
 
+    public LiteralTest() throws IOException {
+        g = getGrammar();
+    }
 
     public void testDecimal() throws Exception {
-        runTest(getGrammar(), "0");
-        runTest(getGrammar(), "2");
-        runTest(getGrammar(), "0372");
-        runTest(getGrammar(), "0xDada_Cafe");
-        runTest(getGrammar(), "1996");
-        runTest(getGrammar(), "0x00_FF__00_FF");
+        runTest("0");
+        runTest("2");
+        runTest("0372");
+        runTest("0xDada_Cafe");
+        runTest("1996");
+        runTest("0x00_FF__00_FF");
+        runTest("0b1111_0000");
+        runTest("0B1111_1111");
+        runTest("0xC0B0L");
+        runTest("0l");
+        runTest("0777L");
     }
 
     private CompiledGrammar getGrammar() throws IOException {
         return new GaGrammar().parse(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("java.napa"), "UTF-8"));
     }
 
-    private void runTest(final CompiledGrammar g, final String source) throws IOException {
+    private void runTest(final String source) throws IOException {
         Set<SyntaxTreeNode> forest = new Parser(g).parse("Literal", new StringReader(source), 100);
         Assert.assertEquals(1, forest.size());
         SyntaxTreeNode tree = forest.iterator().next();
