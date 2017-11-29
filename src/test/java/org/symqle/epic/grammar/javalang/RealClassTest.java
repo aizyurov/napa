c480272d1cf1e5118160bd394b4f87103f0f2e98
lvovich <lvovich@yandex-team.ru>
2017-11-29 20:05:11 +0300

    debug

--- src/test/java/org/symqle/epic/grammar/javalang/RealClassTest.java
+++ src/test/java/org/symqle/epic/grammar/javalang/RealClassTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import org.symqle.epic.gparser.Parser;
 import org.symqle.epic.gparser.SyntaxTree;
 
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -39,6 +40,9 @@ public class RealClassTest extends TestCase {
         {
             List<SyntaxTree> forest = g.parse("CompilationUnit", new InputStreamReader(getClass().getClassLoader().getResourceAsStream("EnvironmentDeploymentService.txt")), 1000);
             Assert.assertEquals(1, forest.size());
+            SyntaxTree tree = forest.get(0);
+            tree.print(new FileOutputStream("true2"));
+
         }
     }
 
